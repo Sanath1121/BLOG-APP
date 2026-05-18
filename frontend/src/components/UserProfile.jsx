2f205f0b19bfd2ad76a9034145a6bd775700aeb3
Vemula Sanath Chandra <sanathchandra2005@gmail.com>
2026-05-18 11:20:01 +0530

    Fix frontend API host to current backend deployment

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -29,7 +29,7 @@ function UserProfile() {
     const getArticles = async () => {
       setLoading(true);
       try {
-        const res = await axios.get("https://blog-app-q882.onrender.com/user-api/articles", { withCredentials: true });
+        const res = await axios.get("https://blog-app-1-jrys.onrender.com/user-api/articles", { withCredentials: true });
 
         setArticles(res.data.payload);
       } catch (err) {
